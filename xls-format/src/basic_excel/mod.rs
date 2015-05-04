5fee1f30a4edbe9eb9223a409e4a8c437f2a9d7f
Josiah Bruner <josiah@programmer.net>
2015-05-04 15:44:43 -0400

    Functional (buildable) skeleton

--- xls-format/src/basic_excel/mod.rs
+++ xls-format/src/basic_excel/mod.rs
@@ -10,21 +10,31 @@ struct Basic_Excel {
 
 impl Basic_Excel {
     
+    pub fn new() -> Basic_Excel {
+        return Basic_Excel{file_: Compound_File, workbook_: Workbook, worksheets_: vec![Worksheet::Worksheet], yesheets_: vec![Basic_Excel_Worksheet::new()]};
+    }
+    
     // File functions
-    pub fn new(sheets: i32) {
+    pub fn new_with_sheets(sheets: i32) {
         
     }
     
     pub fn load<T>(filename: T) -> bool {
+        let mut result = false;
         
+        return result;
     }
     
     pub fn save() -> bool {
+        let mut result = false;
         
+        return result;
     }
     
     pub fn save_as<T>(filename: T) -> bool {
+        let mut result = false;
         
+        return result;
     }
     
     pub fn close() {
@@ -33,19 +43,27 @@ impl Basic_Excel {
     
     // Worksheet functions
     pub fn get_total_worksheets() -> i32 {
+        let mut result = 0;
         
+        return result;
     }
     
     pub fn get_worksheet<T>(sheet_index: T) -> Basic_Excel_Worksheet {
+        let mut result = Basic_Excel_Worksheet::new();
         
+        return result;
     }
     
     pub fn add_worksheet<T>(sheet_index: T) -> Basic_Excel_Worksheet {
+        let mut result = Basic_Excel_Worksheet::new();
         
+        return result;
     }
     
     pub fn delete_worksheet<T>(sheet_index: T) -> bool {
+        let mut result = false;
         
+        return result;
     }
     
     pub fn get_ansi_sheet_name(sheet_index: i32) {
@@ -53,26 +71,35 @@ impl Basic_Excel {
     }
     
     pub fn get_unicode_sheet_name(sheet_index: i32) -> String {
+        let mut result = String::new();
         
+        return result;
     }
     
     pub fn get_sheet_name(sheet_index: i32) -> (bool, String) {
+        let mut result = false;
+        let mut name = String::new();
         
+        return (result, name);
     }
     
     pub fn rename_worksheet_from_index(sheet_index: i32, to: &str) -> bool {
+        let mut result = false;
         
+        return result;
     }
 
     pub fn rename_worksheet_from_str(from: &str, to: &str) -> bool {
+        let mut result = false;
         
+        return result;
     }
     
 }
 
 struct Basic_Excel_Worksheet {
     pub excel_: Basic_Excel,
-    pub sheet_index: i32,
+    pub sheet_index_: i32,
     pub max_rows_: i32,
     pub max_cols_: i32,
     pub cells_: Vec<Basic_Excel_Cell>,
@@ -80,7 +107,9 @@ struct Basic_Excel_Worksheet {
 }
 
 impl Basic_Excel_Worksheet {
-    
+    pub fn new() -> Basic_Excel_Worksheet {
+        return Basic_Excel_Worksheet{excel_: Basic_Excel::new(), sheet_index_: 0, max_rows_: 0, max_cols_: 0, cells_: vec![Basic_Excel_Cell], col_infos_: Worksheet::Col_Infos};
+    }
 }
 
 struct Compound_File;
